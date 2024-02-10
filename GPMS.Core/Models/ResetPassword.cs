4953e49a0713b3406011271b165803b81302c228
Leen Odeh <leenodeh287@gmail.com>
2024-02-10 17:14:23 +0300

    class ResetPassword implemented

--- GPMS.Core/Models/ResetPassword.cs
+++ GPMS.Core/Models/ResetPassword.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GPMS.Core.Models
 {
-    internal class ResetPassword
+    public class ResetPassword
     {
+        [Compare("Password", ErrorMessage = "Password and Confirmation Passowrd dont match.")]
+        public string ConfirmPassword { get; set; }
+        public string Token { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string Email { get; set; }
     }
 }
