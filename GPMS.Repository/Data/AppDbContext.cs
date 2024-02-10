558591bcf7108b0370767a47923d1ba027ec2293
Leen Odeh <leenodeh287@gmail.com>
2024-02-10 12:00:39 +0300

    Create AppDbContext

--- GPMS.Repository/Data/AppDbContext.cs
+++ GPMS.Repository/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace GPMS.Repository.Data
 {
-    public class AppDbContext
+    public class AppDbContext :DbContext
     {
+        public AppDbContext(DbContextOptions options):base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
