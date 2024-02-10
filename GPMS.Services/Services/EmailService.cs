d679c3646c73134147564e0dff1dbd2baeaf1ccb
Leen Odeh <leenodeh287@gmail.com>
2024-02-10 17:13:52 +0300

    add ResetPassword.cs

--- GPMS.Services/Services/EmailService.cs
+++ GPMS.Services/Services/EmailService.cs
@@ -1,15 +1,61 @@
-﻿using GPMS.Core.Models;
-using MimeKit;
+﻿using MimeKit;
 using MailKit.Net.Smtp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using SmtpClient = MailKit.Net.Smtp.SmtpClient;
 using GPMS.Core.Interfaces;
+using GPMS.Core.Models;
 
-namespace GPMS.Services.Services
+namespace Infrastructure.GPMS.Services
 {
     public class EmailService : IEmailService
     {
+        private readonly EmailConfiguration _emailConfig;
+        public EmailService(EmailConfiguration emailConfig) => _emailConfig = emailConfig;
+
+
         public void SendEmail(Message message)
         {
-            throw new NotImplementedException();
+            var emailMessage = CreateEmailMessage(message);
+            Send(emailMessage);
+        }
+
+        private MimeMessage CreateEmailMessage(Message message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress("email", _emailConfig.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
+
+            return emailMessage;
+        }
+
+        private void Send(MimeMessage mailMessage)
+        {
+            using var client = new SmtpClient();
+            try
+            {
+                client.Connect(_emailConfig.SmtpServer, _emailConfig.Port, true);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.Authenticate(_emailConfig.UserName, _emailConfig.Password);
+
+                client.Send(mailMessage);
+            }
+            catch
+            {
+                //log an error message or throw an exception or both.
+                throw;
+            }
+            finally
+            {
+                client.Disconnect(true);
+                client.Dispose();
+            }
         }
     }
 }
