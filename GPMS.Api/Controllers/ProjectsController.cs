4b26a98ffe21043fec1ac771a98d0428e8792e04
Leen Odeh <leenodeh287@gmail.com>
2024-02-11 18:48:39 +0300

    get & get by id implement

--- GPMS.Api/Controllers/ProjectsController.cs
+++ GPMS.Api/Controllers/ProjectsController.cs
@@ -1,6 +1,7 @@
 ﻿using GPMS.Repository.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GPMS.Api.Controllers
 {
@@ -11,9 +12,26 @@ namespace GPMS.Api.Controllers
         private readonly AppDbContext _context;
         public ProjectsController(AppDbContext context)
         {
-            
+            _context = context;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> AllItems()
+        {
+            var items = await _context.Projects.ToListAsync();
+            return Ok(items);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> AllItems(int id)
+        {
+            var item = await _context.Projects.SingleOrDefaultAsync(x => x.Id == id);
+            if (item == null)
+            {
+                return NotFound($"Item Code {id} not exists!");
+            }
+            return Ok(item);
+        }
 
 
 
