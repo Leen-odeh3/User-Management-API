dfd8c05b7a7f4c0176c3937b4a9cebe02eb89260
Leen Odeh <leenodeh287@gmail.com>
2024-02-11 18:34:50 +0300

    Project Table

---
+++ GPMS.Core/Models/Project.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPMS.Core.Models
+{
+    public class Project
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Year{ get; set; }
+
+        public byte[]? Images { get; set; }
+
+
+    }
+}
