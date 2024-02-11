aece69df19b71b542cfa08268b3a98040cdeb3c2
Leen Odeh <leenodeh287@gmail.com>
2024-02-11 18:47:09 +0300

    create ctor

---
+++ GPMS.Api/Controllers/ProjectsController.cs
@@ -0,0 +1,21 @@
+﻿using GPMS.Repository.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GPMS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProjectsController(AppDbContext context)
+        {
+            
+        }
+
+
+
+
+    }
+}
