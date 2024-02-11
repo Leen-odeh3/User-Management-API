c3355e80238da5d21c74753fabd972854af125d7
Leen Odeh <leenodeh287@gmail.com>
2024-02-11 18:57:19 +0300

    implement all endpoint

--- GPMS.Api/Controllers/ProjectsController.cs
+++ GPMS.Api/Controllers/ProjectsController.cs
@@ -1,4 +1,5 @@
-﻿using GPMS.Repository.Data;
+﻿using GPMS.Core.Models;
+using GPMS.Repository.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -33,6 +34,83 @@ namespace GPMS.Api.Controllers
             return Ok(item);
         }
 
+        [HttpGet("Project/Department/{deptid}")]
+        public async Task<IActionResult> AllItemsWithCategory(int deptid)
+        {
+            var item = await _context.Projects.Where(x => x.DeptID == deptid).ToListAsync();
+            if (item == null)
+            {
+                return NotFound($"Category Id {deptid} has no items!");
+            }
+            return Ok(item);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddItem([FromForm] Project project)
+        {
+            using var stream = new MemoryStream();
+            await project.Images.CopyToAsync(stream);
+
+
+            var item = new Project
+            {
+                Title= project.Title,
+                Description = project.Description,
+                Year = project.Year,
+                DeptID= project.Id,
+                Images = stream.ToArray()
+            };
+            await _context.Projects.AddAsync(item);
+            await _context.SaveChangesAsync();
+            return Ok(item);
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProject(int id)
+        {
+            var item = await _context.Projects.SingleOrDefaultAsync(x => x.Id == id);
+            if (item == null)
+            {
+                return NotFound($"  item id {id} not exists !");
+            }
+            _context.Projects.Remove(item);
+            await _context.SaveChangesAsync();
+            return Ok(item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProject(int id, [FromForm] Project mdl)
+        {
+            var item = await _context.Projects.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound($"Item with id {id} not found!");
+            }
+
+            var isDepartmentExists = await _context.Departments.AnyAsync(x => x.Id == mdl.DeptID);
+            if (!isDepartmentExists)
+            {
+                return NotFound($"Department with id {mdl.DeptID} not found!");
+            }
+
+            if (mdl.Images != null)
+            {
+                using var stream = new MemoryStream();
+                await mdl.Images.CopyToAsync(stream);
+                item.Images = stream.ToArray();
+            }
+
+            item.Title = mdl.Title;
+            item.Description = mdl.Description;
+            item.Year = mdl.Year;
+            item.DeptID = mdl.DeptID;
+
+            await _context.SaveChangesAsync();
+            return Ok(item);
+        }
 
 
     }
